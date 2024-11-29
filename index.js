d400905c0b42ae21becc8f0e89cde66611970e67
SolaKabuta <solakabuta@gmail.com>
2024-11-29 12:30:51 +0100

    style: exposition map commented

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
-let a = 20;
-let b = 15;
-let c = 10;
-let d = 5;
+// const pass = 1234;
 
-let note = 12;
+// console.log(pass);
 
-if(note >= a) {
-    console.log("Excellent");
-}else if (note >= b) {
-    console.log("Bien");
-}else if (note >= c) {
-    console.log("Passable");
-}else if (note >= d) {
-    console.log("Echec");
-}
+// if (pass === 1234) {
+//   console.log("Acces autorise");
+// } else {
+//     console.log("Acces refuse");
+//
+
+// const lundi = 1
+// const mardi = 2
+// const mercredi = 3
+// const jeudi = 4
+// const vendredi = 5
+// let samedi = "weekend"
+// let dimanche = "weekend"
+
+
+// console.log(dimanche, samedi)
+
+// let temp = 18
+
+// if (temp <= 0) {
+//     console.log("Il fait très froid")
+// } else if (temp >= 0 && temp < 20) {
+//     console.log("Il fait frais")
+// } else if (temp = 20) {
+//     console.log("Il fait chaud")
+// }
+
+let y = 2, x = 3, z = 4
+
+console.log(x)
