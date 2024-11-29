bf85198a71be4e383889bf05f61b5573a9aa7fd8
SolaKabuta <solakabuta@gmail.com>
2024-11-29 11:33:58 +0100

    style: nav bar logo added on every pages

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-let x = 12
+let a = 20;
+let b = 15;
+let c = 10;
+let d = 5;
+
+let note = 12;
+
+if(note >= a) {
+    console.log("Excellent");
+}else if (note >= b) {
+    console.log("Bien");
+}else if (note >= c) {
+    console.log("Passable");
+}else if (note >= d) {
+    console.log("Echec");
+}
